887f2f532e7e4f384a8dcf37db00f71e4f5c553d
Amir Barylko <amir@barylko.com>
2016-04-19 21:03:26 -0500

    P54 working

--- problem_054.fsx
+++ problem_054.fsx
@@ -4,35 +4,35 @@ open System.IO
 type Player = | Player1 | Player2
 
 let cards = ['0';'1';'2'; '3'; '4'; '5'; '6'; '7'; '8'; '9'; 'T'; 'J'; 'Q'; 'K'; 'A']
+let cardOrder c = cards |> List.findIndex ((=) c)
+let highToLow = cardOrder >> (-) 15
 
-let cardOrder  c = cards |> List.findIndex ((=) c)
-let highToLow' = cardOrder >> (-) 15
-let highToLow (c, _) = c |> highToLow'
+let isInfix (hand: char array) =
+  let cardss = cards |> Array.ofList |> String
 
-let freq = Array.groupBy fst >> Array.map (fun (n, v) -> (n, v.Length)) >> Array.sortBy (fun (c, l) -> 100 * l + highToLow' c)
-
-let isInfix (hand: char array)  =
-  let starts = hand.[0] |> cardOrder
-  let ends   = hand.[4] |> cardOrder
-  ends - starts = 4
+  hand
+  |> String
+  |> fun s -> cardss.Contains(s)
+  |> (&&) (hand.Length = 5)
 
 let handValue str =
-  let hand      = str |> Array.map (fun (s:string) -> (s.[0], s.[1]))
-  let cardFreq  = freq hand
-  let values    = hand |> Array.map fst |> Array.sortBy cardOrder
+  let freq      = Array.groupBy fst >> Array.map (fun (n, v) -> (n, v.Length)) >> Array.sortBy (fun (c, l) -> 100 * (5-l) + highToLow c)
+  let hand      = str    |> Array.map (fun (s:string) -> (s.[0], s.[1]))
+  let values    = hand   |> Array.map fst |> Array.sortBy cardOrder
   let valuesH2L = values |> Array.rev |> String
+  let suits     = hand   |> Array.map snd
 
-  let isFlush = hand |> Array.map snd |> Array.distinct |> Array.length |> (=) 1 // same suit
-  let isRoyal = values = [|'T'; 'J'; 'Q'; 'K'; 'A'|]
+  let isFlush    = suits  |> Array.distinct |> Array.length |> (=) 1 // same suit
   let isStraight = values |> isInfix // consecutive
+  let isRoyal    = values = [|'T'; 'J'; 'Q'; 'K'; 'A'|]
 
-  match cardFreq with
-  | h when isFlush && isRoyal     -> "900000"
-  | h when isFlush && isStraight  -> "8" + valuesH2L
+  match freq hand with
+  | _ when isFlush && isRoyal     -> "900000"
+  | _ when isFlush && isStraight  -> "8" + valuesH2L
   | [|(c1, 4); (c2, 1)|]          -> "7000" + ([|c1; c2|] |> String)
   | [|(c1, 3); (c2, 2)|]          -> "6000" + ([|c1; c2|] |> String)
-  | h when isFlush                -> "5" + valuesH2L
-  | h when isStraight             -> "4" + valuesH2L
+  | _ when isFlush                -> "5" + valuesH2L
+  | _ when isStraight             -> "4" + valuesH2L
   | [|(c1, 3); (c2, 1); (c3, 1)|] -> "300" + ([|c1; c2; c3|] |> String)
   | [|(c1, 2); (c2, 2); (c3, 1)|] -> "200" + ([|c1; c2; c3|] |> String)
   | [|(c1, 2); (c2, 1); (c3, 1); (c4, 1)|] -> "10" + String(c1, 1) + ([|c2; c3; c4|] |> String)
@@ -48,14 +48,9 @@ let playHand line =
   | ((a, b)::xs) when cardOrder a = cardOrder b -> winner xs
   | _ -> Player2
 
-  let won = List.zip h1 h2 |> winner
-
-  //printf "%s\n" (line |> String.concat " ")
-  //printf "%O --- %O (%O)\n" h1 h2 won
-  won
+  List.zip h1 h2 |> winner
 
 File.ReadAllLines(@"p054_poker.txt")
-//|> Seq.take 5
 |> Seq.map (fun s -> s.Split ' ' |> playHand)
 |> Seq.filter ((=) Player1)
 |> Seq.length
